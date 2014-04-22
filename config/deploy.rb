1681740e3c7d970c1cc6921d4572b1e528fca57a
Michiel Sikkes <michiel.sikkes@gmail.com>
2014-04-22 16:07:14 +0200

    Use var for secret key base

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :ssh_options, {
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml .rbenv-vars}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
